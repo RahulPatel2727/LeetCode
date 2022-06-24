804a90371f8c63c410cc37640251ac6807a3ee45
Rahul Patel <rahul.patel19042000@gmail.com>
2022-06-24 15:27:19 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (86.13%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,10 +1,17 @@
 class Solution {
     public int fib(int n) {
-        return fibo(n);
-    }
-    public static int fibo(int n){
         if(n<=1)
             return n;
-        return fibo(n-1)+fibo(n-2);
+        int res=1,b=0;
+        for(int i=2;i<=n;i++){
+            res=res+b;
+            b=res-b;
+        }
+        return res;
     }
+    // public static int fibo(int n){
+    //     if(n<=1)
+    //         return n;
+    //     return fibo(n-1)+fibo(n-2);
+    // }
 }
