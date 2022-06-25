7fd7285a3f6775d8eacbdbce120a961735d9bd95
Rahul Patel <rahul.patel19042000@gmail.com>
2022-06-25 07:27:43 +0530

    Added solution - LeetHub

---
+++ Palindrome String - GFG/palindrome-string.java
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.isPalindrome(S));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isPalindrome(String S) {
+        // code here
+        if(palindrome(S,0,S.length()-1))
+            return 1;
+        else 
+            return 0;
+    }
+    //  i want to use recursion because i am currently learning it
+    static boolean palindrome(String s,int f,int l){
+        if(f>=l)
+            return true;
+        return ((s.charAt(f)==s.charAt(l)) && palindrome(s,f+1,l-1));
+    }
+};
