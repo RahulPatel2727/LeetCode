ad94bc1dd4260979731c05548a6b8fd40143499e
Rahul Patel <rahul.patel19042000@gmail.com>
2022-06-25 09:09:36 +0530

    Time: 12 ms (44.56%), Space: 48 MB (16.07%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,21 +1,39 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
-        s=s.toLowerCase();
-        String res="";
-        for(int i=0;i<s.length();i++){
-            if((s.charAt(i)>=97 && s.charAt(i)<=122) || (s.charAt(i)>=48 && s.charAt(i)<=57)){
-                res+=s.charAt(i);
+        int st=0,en=s.length()-1;
+        for(int i=0;!(st>=en);i++){
+            boolean b=true,c=true;
+            if(!( (s.charAt(st)>=97 && s.charAt(st)<=122) || (s.charAt(st)>=48 && s.charAt(st)<=57) || (s.charAt(st)>=65 && s.charAt(st)<=90) )){
+                st++;
+                b=false;
+            }
+            if(!( (s.charAt(en)>=97 && s.charAt(en)<=122) || (s.charAt(en)>=48 && s.charAt(en)<=57) || (s.charAt(en)>=65 && s.charAt(en)<=90) )){
+                en--;
+                c=false;
+            }
+            if(b && c){
+                if(!((""+s.charAt(st)).equalsIgnoreCase(""+s.charAt(en))))
+                    return false;
+                else{
+                    st++;en--;
+                }
             }
         }
-        
-        int f=0;int l=res.length()-1;
-        System.out.println(res);
-        while(!(f>=l)){
-            if(res.charAt(f)!=res.charAt(l))
-                return false;
-            f++;l--;
-        }
         return true;
+        // s=s.toLowerCase();
+        // String res="";
+        // for(int i=0;i<s.length();i++){
+        //     if((s.charAt(i)>=97 && s.charAt(i)<=122) || (s.charAt(i)>=48 && s.charAt(i)<=57)){
+        //         res+=s.charAt(i);
+        //     }
+        // }
+        // int f=0;int l=res.length()-1;
+        // System.out.println(res);
+        // while(!(f>=l)){
+        //     if(res.charAt(f)!=res.charAt(l))
+        //         return false;
+        //     f++;l--;
+        // }
+        // return true;
     }
 }
