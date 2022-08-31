72df2ad4b8b0c1b4816757a55c9fe703ccaee575
Rahul Patel <rahul.patel19042000@gmail.com>
2022-08-31 23:02:11 +0530

    Time: 3 ms (25.95%), Space: 48.6 MB (57.68%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -7,20 +7,22 @@ class Solution {
 		int []right= new int[arr.length];
 		left[0]=arr[0];
 		for(int i=1;i<arr.length;i++){
-			if(arr[i]>left[i-1])
-				left[i]=arr[i];
-			else
-				left[i]=left[i-1];
+            left[i]=(arr[i]>left[i-1])?arr[i]:left[i-1];
+			// if(arr[i]>left[i-1])
+			// 	left[i]=arr[i];
+			// else
+			// 	left[i]=left[i-1];
 		}
 
 		right[arr.length-1]=arr[arr.length-1];
 		for(int i=arr.length-2;i>=0;i--){
-			if(arr[i]>right[i+1]){
-				right[i]=arr[i];
-			}
-			else{
-				right[i]=right[i+1];
-			}
+            right[i]=(arr[i]>right[i+1])?arr[i]:right[i+1];
+			// if(arr[i]>right[i+1]){
+			// 	right[i]=arr[i];
+			// }
+			// else{
+			// 	right[i]=right[i+1];
+			// }
 		}
 
 		int res = 0;
