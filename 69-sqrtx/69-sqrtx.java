745dbf0f8a79cd92e719555d889947e7c31c0708
Rahul Patel <rahul.patel19042000@gmail.com>
2022-09-06 19:53:23 +0530

    Time: 2 ms (83.47%), Space: 42.2 MB (6.35%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,11 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)sqrt(x);
+        return sqrt(x);
     }
-    private static long sqrt(int num){
-        long e=0;
-        while((e*e)<=num)
-            e++;
-        return e-1;
+    private static int sqrt(int num){
+        if(num==0 || num==1)
+            return num;
+        int s=0,e=num;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(mid==num/mid)
+                return mid;
+            else if(mid > num / mid){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+            // s++;e--;
+        }
+        return e;
     }
 }
