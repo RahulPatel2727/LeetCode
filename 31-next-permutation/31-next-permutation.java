153ed627eddd00fb9e9f8bdb2f5c657fcec56e61
Rahul Patel <rahul.patel19042000@gmail.com>
2022-09-08 18:51:23 +0530

    Time: 1 ms (92.25%), Space: 44.2 MB (16.16%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,7 +1,5 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        if(nums.length==1)
-            return;
         point(nums);
     }
     private static void point(int []arr){
