fc1b03b59eaef2c5cb1f2641d49433b597d8e07c
Rahul Patel <rahul.patel19042000@gmail.com>
2022-09-16 00:10:58 +0530

    Time: 2 ms (47.07%), Space: 43.3 MB (85.74%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,8 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        return buySell(prices);
+        return buySell1(prices);
     }
-    private static int buySell(int []arr){
+//     best solution
+    public static int buySell1(int []arr){
+        int count=0;
+        for(int i=1;i<arr.length;i++){
+            if ( arr[i] > arr[i - 1] ) {
+                count += arr[i] - arr[i-1];
+            }
+        }
+        return count;
+    }
+//     brute force approtch
+    private static int buySell2(int []arr){
         int res=0;
         int c=0;
         int k=0;boolean b=false;
