ef750ba230dc87cc4c2f7b520a94b88252972d82
Rahul Patel <rahul.patel19042000@gmail.com>
2022-09-19 22:39:28 +0530

    Time: 18 ms (7.25%), Space: 41.7 MB (86.45%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,32 +1,36 @@
 class Solution {
     public int longestPalindrome(String s) {
-        int res=0;
-      boolean odd=false;
-      Map<Character, Integer> hashMap;
-      hashMap=getFreq(s);
-      
-      for(Map.Entry<Character,Integer> entry:hashMap.entrySet()){
-        if(entry.getValue()%2==0)
-          res+=entry.getValue();
-        else if(entry.getValue()>1 && (entry.getValue()-1)%2==0){
-          res+=entry.getValue()-1;
-          odd=true;
-        } 
-        else 
-          odd=true; 
-      }
-      if(odd)
-        res++;
-      return res;
-      
+        if(s.length()==1)
+            return 1;
+        // s=s.toLowerCase();
+        return check(s);
     }
-  private Map<Character, Integer> getFreq(String s){
-    Map<Character, Integer> hashMap=new HashMap<>();
-    for(Character letter:s.toCharArray()){
-        int freq=hashMap.getOrDefault(letter,0)+1;
-        hashMap.put(letter, freq);
+    private static int check(String s){
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i =0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        if(map.size()==1){
+            return map.get(s.charAt(0));
+        }
+        int one=0,res=0,odd=0,maxOdd=0;
+        for(Character key:map.keySet()){
+            int t=map.get(key);
+            if(t==1){
+                one++;
+            }
+            if(t%2!=0){
+                odd++;
+                if(t>maxOdd){
+                    maxOdd=t;
+                }
+            }
+            int temp = (t/2);
+            res += (temp*2);
+        }
+        if(one > 0 || odd>=1)
+            res++;
+        return res;
     }
-    return hashMap;
-  }
-  
+   
 }
