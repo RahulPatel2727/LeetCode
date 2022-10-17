67b864ed2fbd360f56884e93709bddacc60a3ed6
Rahul Patel <rahul.patel19042000@gmail.com>
2022-10-17 23:16:21 +0530

    Time: 39 ms (43.18%), Space: 54.1 MB (39.35%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,52 +1,23 @@
 class Solution {
     public String frequencySort(String s) {
-        String str = s;
-        String [] a = new String[26];
-        String [] A = new String[26];
-        String [] num = new String[10];
-        for(int i = 0; i<26 ; i++){
-            if(i<10){
-                num[i]="";
-            }
-            a[i]="";
-            A[i]="";
-        }
-        for(int i = 0; i<str.length(); i++){
-            char ch = str.charAt(i);
-            if(Character.isUpperCase(ch)){
-                A[ch-'A'] += ch;
-            }
-            else if(Character.isLowerCase(ch)){
-                a[ch-'a'] += ch;
-            }
-            else{
-                 num[ch-'0'] += ch;
-            }
-        }
+//         this code is of someone else 
+        // Count the occurence on each character
+	HashMap<Character, Integer> cnt = new HashMap<>();
+	for (char c : s.toCharArray()) {
+		cnt.put(c, cnt.getOrDefault(c, 0) + 1);
+	}
+	
+	// Sorting
+	List<Character> chars = new ArrayList(cnt.keySet());
+	Collections.sort(chars, (a, b) -> (cnt.get(b) - cnt.get(a)));
 
-        HashMap<String, Integer> map = new HashMap<>();
-
-        for(int i=0;i<a.length;i++) {
-            if(a[i].length()>0)
-                map.put(a[i], a[i].length());
-        }
-
-        for (int i = 0; i < A.length; i++){
-            if(A[i].length()>0)
-                map.put(A[i],A[i].length());
-        }
-
-        for(int i=0;i<num.length;i++) {
-            if(num[i].length()>0)
-                map.put(num[i], num[i].length());
-        }
-        int len = map.size();
-        String res = "";
-        for (int i = 0; i < len; i++) {
-            String key = Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
-            res += key;
-            map.remove(key);
-        }
-        return res;
+	// Build string
+	StringBuilder sb = new StringBuilder();
+	for (Object c : chars) {
+		for (int i = 0; i < cnt.get(c); i++) {
+			sb.append(c);
+		}
+	}
+	return sb.toString();
     }
 }
