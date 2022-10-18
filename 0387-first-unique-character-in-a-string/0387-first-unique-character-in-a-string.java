688c9dfa8d9eebaa481897cfb68dbd94eee67f51
Rahul Patel <rahul.patel19042000@gmail.com>
2022-10-19 00:42:55 +0530

    Time: 16 ms (71.44%), Space: 48.4 MB (68.66%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -3,15 +3,15 @@ class Solution {
         int len = s.length();
         if(len == 1)
             return 0;
-        boolean []vis =new boolean[26];
+        int []frq =new int[26];
         for(int i=0;i<len;i++){
             char ch = s.charAt(i);
-            if(!vis[ch-'a']){
-                vis[ch-'a']=true;
-                String t = s.substring(i+1);
-                if(t.indexOf(ch) == -1){
-                    return i;
-                }
+            frq[ch-'a']++;
+        }
+        for(int i=0;i<len;i++){
+            char ch = s.charAt(i);
+            if(frq[ch-'a']==1){
+                return i;
             }
         }
         return -1;
