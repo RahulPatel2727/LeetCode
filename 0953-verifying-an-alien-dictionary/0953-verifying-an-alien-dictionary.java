b637cb0a75a3760bc6a11bc914a11d8793a113d3
Rahul Patel <rahul.patel19042000@gmail.com>
2022-10-19 20:19:54 +0530

    Time: 1 ms (83.88%), Space: 42.2 MB (74.96%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -1,41 +1,28 @@
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-        return isSorted(words, order);
-    }
-    public static boolean isSorted(String[] words, String order) {
-        boolean f = false;
-        for(int i = words.length-1;i>0;i--){
-            String g = words[i];
-            String s = words[i-1];
-            if(g.equals(s)){
-                continue;
-            }
-            boolean equal = false;
-            int min = (g.length()<s.length()) ? g.length() : s.length();
-            for (int j = 0; j < min; j++) {
-                int gg = order.indexOf(g.charAt(j));
-                int ss = order.indexOf(s.charAt(j));
-                if(gg>ss){
-                    f = true;
-                    break;
-                }
-                else if(gg<ss){
+        int[] orderMap = new int[26];
+        for (int i = 0; i < order.length(); i++){
+            orderMap[order.charAt(i) - 'a'] = i;
+        }
+
+        for (int i = 0; i < words.length - 1; i++) {
+
+            for (int j = 0; j < words[i].length(); j++) {
+                // If we do not find a mismatch letter between words[i] and words[i + 1],
+                // we need to examine the case when words are like ("apple", "app").
+                if (j >= words[i + 1].length()) 
                     return false;
+
+                if (words[i].charAt(j) != words[i + 1].charAt(j)) {
+                    int currentWordChar = words[i].charAt(j) - 'a';
+                    int nextWordChar = words[i + 1].charAt(j) - 'a';
+                    if (orderMap[currentWordChar] > orderMap[nextWordChar]) 
+                        return false;
+                    // if we find the first different letter and they are sorted,
+                    // then there's no need to check remaining letters
+                    else 
+                        break;
                 }
-                else if(gg == ss){
-                    equal = true;
-                }
-            }
-            
-            // if break then continue
-            if(f){
-                f = false;
-                continue;
-            }
-            
-            // for equal at min length check
-            if(equal && s.length()>g.length()){
-                return false;
             }
         }
         return true;
