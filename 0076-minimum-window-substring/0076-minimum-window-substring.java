3bb76beb9c08d9bc21d4b6e78d9d583a67d7a7d9
Rahul Patel <rahul.patel19042000@gmail.com>
2022-10-22 18:45:57 +0530

    Time: 18 ms (69.23%), Space: 47 MB (43.48%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,79 +1,66 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int[] arr = new int[52];      // creating 52 size array because of upper and lowercase character
-        for(int i = 0; i < t.length(); i++){
-            char ch = t.charAt(i);
-            int idx = -1;
-            if(ch >= 'A' && ch <= 'Z'){
-                idx = t.charAt(i) - 'A' + 26;
-            }else{
-                idx = t.charAt(i) - 'a';
-            }
-            arr[idx]++;   // counting frequency of character of string t
-        }
-        int[] narr = new int[52];    // array for storing frequency of character for string s from index j to i
-        int si = -1;      // starting index of smallest which contains t
-        int ei = -1;     // ending index of smallest which contains t
-        int ans = Integer.MAX_VALUE;   // length of smallest string which contains t
-        
-        int i = 0;   // representing string ending at ith index
-        int j = 0;   // representing string starting at jth index
-        while(i < s.length()){
-            char ch = s.charAt(i);
-            int idx = -1;
-            if(ch >= 'A' && ch <= 'Z'){
-                idx = ch - 'A' + 26;
-            }else{
-                idx = ch - 'a';
-            }
-            narr[idx]++;      //    counting frequence of character of string s from index j to i
-            boolean flag = check(arr, narr);    // checking between index j to i all character of t is present in s or not
-        
-            if(flag == true){         
-					if(i - j + 1 < ans){                   // if present then check its length is less than the previous valid length or not
-                    ans = i - j + 1;  // update length of smallest valid string
-                    si = j;     // update starting index of smallest valid string
-                    ei = i;    // update ending index of smallest valid string
-                }
-                while(j <= i && j < s.length() && flag == true){    // if flag is true then decrease frequency of jth character and move j to next position 
-                    char c = s.charAt(j);
-                    int id = -1;
-                    if(c >= 'A' && c <= 'Z'){
-                        id = c - 'A' + 26;
-                    }else{
-                        id = c - 'a';
-                    }
-                    narr[id]--;      
-                    flag = check(arr, narr); // until the flag is true move j to next position
-                    j++;
-                    if(flag == true){
-                        if(i - j + 1 < ans){     // update starting, ending and ans of valid string
-                            ans = i - j + 1;
-                            si = j;
-                            ei = i;
-                        }
-                    }
-                    
-                }
-                
-            }
-            i++;
-        }
-        
-        if(si == -1 || ei == -1){    // if starting or ending index == -1 then no valid string exist so return empty string
+
+        if (s.length() == 0 || t.length() == 0) {
             return "";
         }
-        String fans = s.substring(si, ei + 1);  // if exist 
-        return fans;
-    }
-    
-    public boolean check(int[] arr, int[] narr){    // checking all characters of t is present in s from index j to i
-        
-        for(int k = 0; k < 52; k++){
-            if(arr[k] > narr[k]){
-                return false;
+
+        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < t.length(); i++) {
+            int count = dictT.getOrDefault(t.charAt(i), 0);
+            dictT.put(t.charAt(i), count + 1);
+        }
+
+        int required = dictT.size();
+
+        // Filter all the characters from s into a new list along with their index.
+        // The filtering criteria is that the character should be present in t.
+        List<Pair<Integer, Character>> filteredS = new ArrayList<Pair<Integer, Character>>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (dictT.containsKey(c)) {
+                filteredS.add(new Pair<Integer, Character>(i, c));
             }
         }
-        return true;
+
+        int l = 0, r = 0, formed = 0;
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+        int[] ans = { -1, 0, 0 };
+
+        // Look for the characters only in the filtered list instead of entire s.
+        // This helps to reduce our search.
+        // Hence, we follow the sliding window approach on as small list.
+        while (r < filteredS.size()) {
+            char c = filteredS.get(r).getValue();
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+
+            // Try and contract the window till the point where it ceases to be 'desirable'.
+            while (l <= r && formed == required) {
+                c = filteredS.get(l).getValue();
+
+                // Save the smallest window until now.
+                int end = filteredS.get(r).getKey();
+                int start = filteredS.get(l).getKey();
+                if (ans[0] == -1 || end - start + 1 < ans[0]) {
+                    ans[0] = end - start + 1;
+                    ans[1] = start;
+                    ans[2] = end;
+                }
+
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
     }
 }
