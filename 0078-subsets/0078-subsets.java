c60ae474cc82a62a32db79ed392bc2e5717d6d2f
Rahul Patel <rahul.patel19042000@gmail.com>
2022-10-24 15:11:13 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (84.71%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,26 +1,21 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        int pow = (int) Math.pow(2,nums.length);
-        for(int i=0;i<pow;i++){
-            List lst=new ArrayList();
-            
-            //for null case
+        List <List< Integer>> res = new ArrayList<>();
+        int loop = (int)Math.pow(2,nums.length);
+        for(int i = 0; i < loop; i++){
+            List<Integer> list = new ArrayList<>();
             if(i==0){
-                list.add(lst);
+                res.add(list);
                 continue;
-            } 
-            
-            // bit are length of the nums if len 4 then   bits -> 0 0 0 0
-            // if bit is set then add that 
-            for(int j=0;j<nums.length;j++){
-                if((i & (1<<j))!=0){
-                    lst.add(nums[j]);
-                }
             }
             
-            list.add(lst);
+            for(int j = 0; j<nums.length; j++){
+                if((i & (1 << j)) != 0){
+                    list.add(nums[j]);
+                }
+            }
+            res.add(list);
         }
-        return list;
+        return res;
     }
 }
