647c90b5b590399dc1649d8d52ee94d1a8b18f7f
Rahul Patel <rahul.patel19042000@gmail.com>
2022-10-25 21:46:57 +0530

    Time: 12 ms (9.73%), Space: 42.3 MB (97.16%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,16 +1,15 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        int loop = (int)Math.pow(2,nums.length);
-        for(int i=0;i<loop; i++){
-            List<Integer> list = new ArrayList<>();
+       List<List<Integer>> res = new ArrayList<>();
+        int pow = (int ) Math.pow(2,nums.length);
+        for(int i=0;i<pow;i++){
+            List list = new ArrayList<>();
             if(i==0){
                 res.add(list);
                 continue;
             }
-            
-            for(int j = 0; j<nums.length;j++){
-                if((i & (1 << j)) !=0 ){
+            for(int j = 0;j<nums.length;j++){
+                if((i & (1<<j))!=0){
                     list.add(nums[j]);
                 }
             }
