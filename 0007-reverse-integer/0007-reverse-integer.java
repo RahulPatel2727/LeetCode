c516bb559a3efee30ad66184fb17b8d574182993
Rahul Patel <rahul.patel19042000@gmail.com>
2022-11-13 10:08:23 +0530

    Time: 2 ms (84.86%), Space: 41.6 MB (37.08%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -7,10 +7,10 @@ class Solution {
         long res = 0;
         while(x!=0){
             res = res*10 + x%10;
-            if(res <= -2147483648 || res >= 2147483647)
-                return 0;
             x/=10;
         }
+        if(res <= -2147483648 || res >= 2147483647)
+                return 0;
         return (int)res*mul;
     }
 }
