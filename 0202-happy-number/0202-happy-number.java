bd125fa4458c5362f42b953b766f17b9cf8671b4
Rahul Patel <rahul.patel19042000@gmail.com>
2022-11-23 01:01:42 +0530

    Time: 23 ms (5.18%), Space: 41.4 MB (42.50%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        long ans = 0;
+        long nn=n;
+        int i = 1000;
+        while(i-->0){
+            long temp = nn;
+            long sum = 0;
+            while(temp!=0){
+                int d = (int)temp%10;
+                sum += d*d;
+                temp/=10;
+            }
+            if(sum==1)
+                return true;
+            nn=sum;
+        }
+        return false;
+    }
+}
