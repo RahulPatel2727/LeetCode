06c1ba1c457f09ae1c11a0a5e02cec409b823242
Rahul Patel <rahul.patel19042000@gmail.com>
2022-12-19 10:11:24 +0530

    Time: 15 ms (34.77%), Space: 45.8 MB (11.67%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,61 +1,18 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        return find(words,k);
+        HashMap<String,Integer> freq=new HashMap<>();
+        for(int i=0;i<words.length;i++)
+        {
+            freq.put(words[i],freq.getOrDefault(words[i],0)+1);
+        }
+        List<String> res = new ArrayList(freq.keySet());
+        //sorting
+        //if two words have the same frequency, then the word with the lower alphabetical order comes first.
+        //else most frequent words will come first
+        Collections.sort(res, (w1, w2) -> freq.get(w1).equals(freq.get(w2)) ?
+                w1.compareTo(w2) : freq.get(w2) - freq.get(w1));
+
+        return res.subList(0, k);
     }
-    public static List<String> find(String [] arr,int k){
-        List<String> list = new ArrayList<>();
-        Map<String, Integer>map = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-        }
-        String [] el = new String[map.size()];
-        int [] frq = new int[map.size()];
-        int v = 0;
-        for(Map.Entry<String , Integer> mp : map.entrySet()){
-            el[v] = mp.getKey();
-            frq[v] = mp.getValue();
-            v++;
-        }
-        for(int i =0;i<el.length;i++){
-            for(int j=i+1;j<el.length;j++){
-                if(frq[i]>frq[j]){
-                    int t = frq[i];
-                    frq[i] = frq[j];
-                    frq[j] = t;
-                    String tt = el[i];
-                    el[i] = el[j];
-                    el[j] = tt;
-                }
-            }
-        }
-        for(int i=el.length-1;i>0;i--){
-            int p1 = i;
-            while(i>0 && frq[i]==frq[i-1]){
-                i--;
-            }
-            if(p1!=i){
-                sort(el,i,p1);
-            }
-        }
-        int ind = el.length-1;
-        while(k-->0){
-            list.add(el[ind]);
-            ind--;
-        }
-        return list;
-    }
-    public static void sort(String [] arr, int st , int en){
-        String  [] temp = new String[en-st+1];
-        int ind = st;
-        for(int i=0;i<temp.length;i++){
-            temp[i] = arr[ind];
-            ind++;
-        }
-        Arrays.sort(temp);
-        ind = en;
-        for(int i=0;i<temp.length;i++){
-            arr[ind] = temp[i];
-            ind--;
-        }
-    }
+    
 }
