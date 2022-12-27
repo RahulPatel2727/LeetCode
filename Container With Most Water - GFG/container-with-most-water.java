4f0b5c2ab74ee5b7ab2d88919a9736c26e8454a1
Rahul Patel <rahul.patel19042000@gmail.com>
2022-12-27 08:28:11 +0530

    Added solution - LeetHub

---
+++ Container With Most Water - GFG/container-with-most-water.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+class GFG {
+	public static void main (String[] args) throws IOException{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine()); // input size of array
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split("\\s+");
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]); // input elements of array
+		    }
+		    Solve T = new Solve();
+            long ans = T.maxArea(arr,n);
+            System.out.println(ans);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solve{
+    
+    long maxArea(int A[], int len){
+        // Code Here
+        int i=0;
+
+      int j=len-1;
+
+      int ans=0;
+
+      while(i<j){
+
+          ans=Math.max(ans,(j-i)*Math.min(A[i],A[j]));
+
+          if(A[i]<A[j])i++;
+
+          else j--;
+
+          
+
+      }
+
+      return ans;
+        
+    }
+    
+}
