d7a6c0043fe1636c6412fa4cf669dfb38b319d6b
Rahul Patel <rahul.patel19042000@gmail.com>
2023-01-03 16:16:06 +0530

    Time: 6 ms (54.45%), Space: 44 MB (35.23%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -3,28 +3,22 @@ class Solution {
         Arrays.sort(candidates);
         List<List<Integer>> ans = new ArrayList<>();
         find(candidates, target, 0, new ArrayList<>(), ans);
-        // List<List<Integer>> a = new ArrayList(ans);
         return ans;
-        
     }
-    public static void find(int [] arr, int val, int ind, List<Integer> ds, List<List<Integer>> ans){
-        // if(ind == arr.length){
-            if(val == 0){
-                ans.add(new ArrayList<>(ds));
-            // }
+    public static void find(int [] arr, int val ,  int ind, List<Integer> ds, List<List<Integer>> ans ){
+        if(val == 0){
+            ans.add(new ArrayList<>(ds));
             return;
         }
         for(int i = ind; i < arr.length; i++){
-            if(i != ind && arr[i] == arr[i-1]){
+            if(i!=ind && arr[i] == arr[i-1]){
                 continue;
             }
             if(val >= arr[i]){
                 ds.add(arr[i]);
-                find(arr, val-arr[i], i+1, ds, ans);
+                find(arr, val - arr[i], i + 1, ds, ans);
                 ds.remove(ds.size()-1);
             }
         }
-        // find(arr,val, ind+1, ds, ans);
     }
-    
 }
