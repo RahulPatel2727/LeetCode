4a1a1141d979b50b139fa5460510d07d3605ef94
Rahul Patel <rahul.patel19042000@gmail.com>
2023-01-08 11:18:20 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (88.91%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -4,16 +4,19 @@ class Solution {
         find3(k,n,1,new ArrayList<Integer>(), ans); 
         return ans;
     }
-    public static void find3(int k, int val, int ind,  List<Integer>ds, List<List<Integer>> ans){
-        if(val == 0 && ds.size()==k){
-            ans.add(new ArrayList<>(ds));
+    public static void find3(int k, int val, int ind,  List<Integer>list, List<List<Integer>> ans){
+        if(val == 0 && list.size()==k){
+            ans.add(new ArrayList<>(list));
             return;
         }
         for(int i = ind; i <= 9; i++){
             if(i <= val ){
-                ds.add(i);
-                find3(k, val - i,i+1, ds, ans);
-                ds.remove(ds.size() - 1);
+                list.add(i);
+                find3(k, val - i,i+1, list, ans);
+            
+                list.remove(list.size() - 1);
+                if(i>val)
+                    break;
             }
         }
     }
