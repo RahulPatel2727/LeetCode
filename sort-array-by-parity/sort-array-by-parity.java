edfb5d266d384dd4003012d849c57a63b5b36fa4
Rahul Patel <rahul.patel19042000@gmail.com>
2023-01-09 22:47:12 +0530

    Time: 1 ms, Memory: 43.4 MB - LeetHub

--- sort-array-by-parity/sort-array-by-parity.java
+++ sort-array-by-parity/sort-array-by-parity.java
@@ -12,7 +12,6 @@ class Solution {
                     int t = odd;
                     odd = even;
                     even = t;
-                    // continue;
                 }
             }
             
