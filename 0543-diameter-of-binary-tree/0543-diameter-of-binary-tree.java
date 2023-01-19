1cc6eba05b2777351625f938d169f6cf5ecdd6ad
Rahul Patel <rahul.patel19042000@gmail.com>
2023-01-19 13:49:57 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (78.03%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,25 +13,30 @@
  *     }
  * }
  */
-class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        return idm(root);
+class Solution 
+{
+    int max;
+    public int getDiameter(TreeNode root)
+    {
+        int h= 0;
+        
+        if(root == null)//base case 
+           return 0;
+        
+        int l= getDiameter(root.left);//recursing down and calculating the Height 
+        int r= getDiameter(root.right);
+        
+        h= Math.max(l,r);//maximum height 
+        
+        max= Math.max(max,l+r+1);//updating the maximum diameter 
+        
+        h=h+1;//increasing heigh covering every node 
+        return h; //returning the maximum height 
     }
-    public static int idm(TreeNode root){
-        if(root == null){
-            return 0;
-        }
-        int ld = idm(root.left);
-        int rd = idm(root.right);
-        int sd = ht(root.left)+ht(root.right)+2;
-        return Math.max(ld,Math.max(rd,sd));
+    public int diameterOfBinaryTree(TreeNode root) 
+    {
+        max= Integer.MIN_VALUE;//identity 
+        getDiameter(root);
+        return max-1; //node=edge+1, i.e; edge=node-1
     }
-    public static int ht(TreeNode root){
-        if(root == null){
-            return -1;
-        }
-        int lh = ht(root.left);
-        int rh = ht(root.right);
-        return Math.max(lh,rh)+1;
-    }
-}
+}//Please do Upvote, It helps a lot
