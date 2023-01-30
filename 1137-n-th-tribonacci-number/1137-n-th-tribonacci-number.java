77d270a6b7cac1d7562aab29c5749a98047bb3b8
Rahul Patel <rahul.patel19042000@gmail.com>
2023-01-30 20:52:26 +0530

    Time: 0 ms (100.00%), Space: 39 MB (80.79%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,26 +1,22 @@
 class Solution {
-    int[] memo;
-    
-    Solution() {
-        // Initialize memo, with -1 as flags for not calculated
-        memo = new int[38]; // 0 through 37
+    public int tribonacci(int n) {
+        int [] memo = new int[38]; // 0 through 37
         memo[0] = 0;
         memo[1] = 1;
         memo[2] = 1;
         for(int i = 3; i < memo.length; i++){
             memo[i] = -1;
         }
+        return tf(n, memo);
     }
-    
-    public int tribonacci(int n) {
-        // If we have already calculated value, return it.
-        if(memo[n] != -1) {
+    public static int tf(int n , int [] memo){
+        if(memo[n] != -1){
             return memo[n];
         }
-        else {
-            int result = tribonacci(n-3) + tribonacci(n-2) + tribonacci(n-1);
-            memo[n] = result;
-            return result;
+        else{
+            int ans = tf(n-1, memo)+tf(n-2, memo)+tf(n-3, memo);
+            memo[n] = ans;
+            return ans;
         }
     }
 }
