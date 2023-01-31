9848d1353836771f0d1896d798b3a5323b9357ba
Rahul Patel <rahul.patel19042000@gmail.com>
2023-01-31 11:30:29 +0530

    Added solution - LeetHub

---
+++ Minimum times A has to be repeated such that B is a substring of it - GFG/minimum-times-a-has-to-be-repeated-such-that-b-is-a-substring-of-it.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String A = read.readLine();
+            String B = read.readLine();
+
+            Solution ob = new Solution();
+            out.println(ob.minRepeats(A,B));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int minRepeats(String A, String B) {
+        // code here
+        StringBuilder sb = new StringBuilder(A);
+        int count = 1;
+        while (sb.length() < B.length()) {
+            sb.append(A);
+            count++;
+        }
+        if (sb.indexOf(B) >= 0) return count;
+        if (sb.append(A).indexOf(B) >= 0) return count + 1;
+        return -1;
+    }
+};
