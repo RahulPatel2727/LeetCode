6c7bb06db5305d8f6b5af35b3d964040ffd87ba6
Rahul Patel <rahul.patel19042000@gmail.com>
2023-02-10 03:09:32 +0530

    Time: 187 ms (85.25%), Space: 55.7 MB (63.93%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.java
+++ 2306-naming-a-company/2306-naming-a-company.java
@@ -1,32 +1,56 @@
 class Solution {
     public long distinctNames(String[] ideas) {
-        // Group idea by their initials.
-        HashSet<String>[] initialGroup = new HashSet[26];
-        for (int i = 0; i < 26; ++i) {
-            initialGroup[i] = new HashSet<>();
+        return count(ideas);
+    }
+    private static long count(String [] arr){
+        Map<Character, HashSet<String>>mp = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            HashSet<String> t = mp.get(arr[i].charAt(0));
+            if(t!=null)
+                t.add(arr[i].substring(1));
+            else
+                t = new HashSet<>(List.of(arr[i].substring(1)));
+            mp.put(arr[i].charAt(0), mp.getOrDefault(arr[i].charAt(0),t));
         }
-        for (String idea : ideas) {
-            initialGroup[idea.charAt(0) - 'a'].add(idea.substring(1));
+//        System.out.println(mp);
+        long ans = 0;
+        if(mp.size()==1){
+            return 0;
         }
-        
-        // Calculate number of valid names from every pair of groups.
-        long answer = 0;
-        for (int i = 0; i < 25; ++i) {
-            for (int j = i + 1; j < 26; ++j) {
-                // Get the number of mutual suffixes.
-                long numOfMutual = 0;
-                for (String ideaA : initialGroup[i]) {
-                    if (initialGroup[j].contains(ideaA)) {
-                        numOfMutual++;
+        HashSet<String> [] sets = new HashSet[mp.size()];
+        int itr = 0;
+        for (Map.Entry<Character, HashSet<String>> it : mp.entrySet()) {
+            sets[itr] = it.getValue();
+            itr++;
+        }
+        for (int i = 0; i < sets.length-1; i++) {
+            HashSet<String>  set1 = sets[i];
+            for (int j = i+1; j < sets.length; j++) {
+                HashSet<String> set2 = sets[j];
+                int c = 0;
+                for(String st : set1){
+                    if(set2.contains(st)){
+                        c++;
                     }
                 }
-
-                // Valid names are only from distinct suffixes in both groups.
-                // Since we can swap a with b and swap b with a to create two valid names, multiple answer by 2.
-                answer += 2 * (initialGroup[i].size() - numOfMutual) * (initialGroup[j].size() - numOfMutual);
+                int l1 = set1.size()-c;
+                int l2 = set2.size()-c;
+                ans += l1 * l2 * 2;
             }
         }
-        
-        return answer;
+        return ans;
     }
 }
+
+// c ine 2 2 2
+
+// c offee 2 2
+
+// l offee 2 2 1
+// s offee  2 2 1
+
+// d onuts 3 2
+// d emon  3 2
+
+// t ime     3 2 
+// t ruck    3 2
