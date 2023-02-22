62564afcf21be30cde19c6939a0fbd8dd59dcc07
Rahul Patel <rahul.patel19042000@gmail.com>
2023-02-22 15:53:57 +0530

    Time: 11 ms (46.12%), Space: 54 MB (10.58%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,14 +1,13 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int l = 0, h = 0, mid;
-        for(int i:weights){
-            l = Math.max(i,l);
-            h += i;
+        for(int it : weights){
+            h += it;
+            l = Math.max(l,it);
         }
-        // low = max of weigth and h = sum of all weights
         while(l < h){
             mid = l + (h - l)/2;
-            if(isTrue(mid,weights,days)){ // if true then we goes to minimise the mid again
+            if(is(mid, weights, days)){
                 h = mid;
             }
             else{
@@ -16,20 +15,16 @@ class Solution {
             }
         }
         return l;
-        
     }
-    
-    
-    // verifying the mid value that can port or not in <= days days
-    public boolean isTrue(int mid, int[] arr,int days){
-        int sum = 0, count = 1;
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
+    public boolean is(int mid, int [] arr, int days){
+        int c = 1, sum = 0;
+        for(int it : arr){
+            sum += it;
             if(sum > mid){
-                count++;
-                sum = arr[i];
+                sum = it;
+                c++;
             }
         }
-        return count<=days;
+        return c<=days;
     }
 }
