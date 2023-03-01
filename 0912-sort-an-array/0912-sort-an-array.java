2844bb7423090ebe6c9d898c539d6a22481a02ba
Rahul Patel <rahul.patel19042000@gmail.com>
2023-03-01 08:27:24 +0530

    Time: 7 ms (96.94%), Space: 52.2 MB (75.99%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,35 +1,33 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        QS(nums, 0, nums.length - 1);
-        return nums;
-    }
-    public static void QS(int [] arr, int s, int e){
-        if(s < e){
-            int pivotIndex = findPivot(arr, s, e);
-            QS(arr, s, pivotIndex);
-            QS(arr, pivotIndex + 1, e);
+        int max = nums[0];
+        int min = max;
+        for(int it : nums){
+            max = Math.max(max, it);
+            min = Math.min(min, it);
         }
+        return countingSort(nums, max, min);
     }
-    public static int findPivot(int []arr, int s, int e){
-        int pivot = arr[s];
-        s = s-1;
-        e = e+1;
-        while(true){
-            do{
-                s++;
-            }while(arr[s]<pivot);
-            do{
-                e--;
-            }while(arr[e]>pivot);
-            if(s < e){
-                swap(arr,s,e);
-            }
-            else return e;
+    public int[] countingSort(int[] arr, int max, int min) {
+        int plus;
+        if (min < 0) {
+            plus = Math.abs(min);
+        } else {
+            plus = 0;
         }
-    }
-    public static void swap(int [] arr, int i, int j){
-        int t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
+        int cL = max + plus + 1;
+        int[] c = new int[cL];
+        for (int i = 0; i < arr.length; i++) {
+            c[arr[i] + plus]++;
+        }
+        for (int i = 1; i < cL; i++) {
+            c[i] = c[i] + c[i - 1];
+        }
+        int[] temp = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            temp[c[arr[i] + plus] - 1] = arr[i];
+            c[arr[i] + plus]--;
+        }
+        return temp;
     }
 }
