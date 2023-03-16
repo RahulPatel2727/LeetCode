1d142bc0414c20b56e9ffb222dce89c5fd7de92b
Rahul Patel <rahul.patel19042000@gmail.com>
2023-03-17 00:18:01 +0530

    Added solution - LeetHub

---
+++ Number of Subarrays of 0's - GFG/number-of-subarrays-of-0s.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG{
+	public static void main(String [] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		while(test-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			int [] arr = new int[N];
+			String [] str = br.readLine().trim().split(" ");
+			for(int i= 0; i < N; i++)
+				arr[i] = Integer.parseInt(str[i]);
+			Solution obj = new Solution();
+			System.out.println(obj.no_of_subarrays(N, arr));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+	long no_of_subarrays(int N, int [] arr) {
+		//Write your code here
+		long z = 0;
+		long ans = 0;
+		for(int it : arr){
+		    if(it == 0){
+		        z++;
+		    }
+		    else{
+		        ans += (z * (z + 1))/2;
+		        z = 0;
+		    }
+		}
+		ans += (z * (z + 1))/2;
+		return ans;
+	}
+}
