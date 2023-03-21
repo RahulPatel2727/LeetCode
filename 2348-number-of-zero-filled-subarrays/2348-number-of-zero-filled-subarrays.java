515de98a048d57993f2d751dad1a74b7555254e6
Rahul Patel <rahul.patel19042000@gmail.com>
2023-03-21 17:02:47 +0530

    Time: 3 ms (99.69%), Space: 60.2 MB (20.69%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -3,13 +3,15 @@ class Solution {
         long ans = 0;
         long zc = 0;
         for(int it = 0; it < nums.length; it ++){
-            while(it<nums.length && nums[it] == 0){
-                zc++; it++;
+            if(nums[it] == 0){
+                zc++;
             }
-            ans += (zc*(zc+1))/2;
-            zc = 0;
+            else{
+                zc = 0;
+            }
+            ans += zc;
         }
-        ans += (zc*(zc+1))/2;
+        // ans += (zc*(zc+1))/2;
         return ans;
     }
 }
