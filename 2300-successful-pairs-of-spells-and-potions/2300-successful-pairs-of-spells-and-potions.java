0c3384b050417817827cfeca989858d79eb18f6e
Rahul Patel <rahul.patel19042000@gmail.com>
2023-04-02 21:24:26 +0530

    Time: 73 ms (74.38%), Space: 60.6 MB (92.98%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -1,16 +1,36 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        // my code
         Arrays.sort(potions);
-		TreeMap<Long, Integer> map = new TreeMap<>();
-        map.put(Long.MAX_VALUE, potions.length);
-		for (int i = potions.length - 1; i >= 0; i--) {
-			map.put((long) potions[i], i);
-		}
-		for (int i = 0; i < spells.length; i++) {
-            long need = (success + spells[i] - 1) / spells[i];
-			spells[i] = potions.length - map.ceilingEntry(need).getValue();
-		}
-		return spells;
+        int p = potions.length, s = spells.length;
+        for(int i=0;i<s; i++){
+            int si = 0, ei = p-1;
+            while(si <= ei){
+                int mid = si + (ei - si) / 2;
+                if(((long) spells[i] * potions[mid]) >= success){
+                    ei = mid - 1;
+                }
+                else{
+                    si = mid + 1;
+                }
+            }
+            spells[i] = p - si;
+        }
+        return spells;
+        
+        
+        // bad solution
+		// Arrays.sort(potions);
+		// TreeMap<Long, Integer> map = new TreeMap<>();
+		// map.put(Long.MAX_VALUE, potions.length);
+		// for (int i = potions.length - 1; i >= 0; i--) {
+		// 	map.put((long) potions[i], i);
+		// }
+		// for (int i = 0; i < spells.length; i++) {
+		// long need = (success + spells[i] - 1) / spells[i];
+		// 	spells[i] = potions.length - map.ceilingEntry(need).getValue();
+		// }
+		// return spells;
         
     }
 }
