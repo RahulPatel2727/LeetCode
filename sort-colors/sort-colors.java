5ac5cebf17d33633ce001bb32b3cf6ff1090cd50
Rahul Patel <rahul.patel19042000@gmail.com>
2023-05-19 18:44:44 +0530

    Time: 0 ms, Memory: 41 MB - LeetHub

--- sort-colors/sort-colors.java
+++ sort-colors/sort-colors.java
@@ -1,24 +1,25 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int si=0, ei=nums.length-1, cur = 0;
-        while(cur <= ei){
-            if(nums[cur]==0){
-                swap(nums, cur, si);
-                cur++;
-                si++;
-            }
-            else if(nums[cur]==2){
-                swap(nums, cur, ei);
-                ei--;
-                // cur++;
-            }
-            else 
-                cur++;
+        int [] ans = countingSort(nums);
+        for(int i=0; i<nums.length; i++){
+            nums[i] = ans[i];
         }
     }
-    public static void swap(int [] arr, int a, int b){
-        int t = arr[a];
-        arr[a] = arr[b];
-        arr[b] = t;
+    public static int [] countingSort(int [] nums){
+        int [] frq = new int[3];
+        for(int i : nums){
+            frq[i]++;
+        }
+        
+        for(int i=1;i<3;i++){
+            frq[i] = frq[i] + frq[i-1];
+        }
+        
+        int [] ans = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            ans[frq[nums[i]]-1] = nums[i];
+            frq[nums[i]] -= 1;
+        }
+        return Arrays.copyOf(ans, ans.length);
     }
 }
