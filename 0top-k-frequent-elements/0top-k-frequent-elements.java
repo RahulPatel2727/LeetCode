e0bef4394b97565652e8f64d89baf667ea1d0381
Rahul Patel <rahul.patel19042000@gmail.com>
2023-05-20 07:02:58 +0530

    Time: 12 ms, Memory: 47.6 MB - LeetHub

---
+++ 0top-k-frequent-elements/0top-k-frequent-elements.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        nums = find(nums,k);
+        return nums;
+    }
+    public static int [] find(int [] arr,int k){
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        int [] el = new int[map.size()];
+        int [] frq = new int[map.size()];
+        int i = 0;
+        for(Map.Entry<Integer,Integer> mp : map.entrySet()) {
+            el[i]=mp.getKey();
+            frq[i]=mp.getValue();
+            i++;
+        }
+        
+        //bubble sort the frequency w.r.t  el
+        for(int j = 0; j<el.length;j++){
+            for(int m = j+1;m<el.length;m++){
+                //SWAPING
+                if(frq[j]>frq[m]){
+                    int t = frq[j];
+                    frq[j] = frq[m];
+                    frq[m] = t;
+                    int tt = el[j];
+                    el[j] = el[m];
+                    el[m] = tt;
+                }
+            }
+        }
+        int len = el.length-1;
+        int st = 0;
+        int [] ans = new int[k];
+        while(k-->0){
+            ans[st] = el[len];
+            st++;
+            len--;
+        }
+        return ans;
+    }
+}
