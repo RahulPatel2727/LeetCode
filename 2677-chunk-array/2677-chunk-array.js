7c39ec23ddc5fd86adf13c8c31ffdb7f24e12912
Rahul Patel <rahul.patel19042000@gmail.com>
2023-05-25 06:25:31 +0530

    Time: 71 ms (23.45%), Space: 44.2 MB (78.31%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+  let chunkedArray = [[]];
+  let temp = [];
+  
+  for (let i = 0; i < arr.length; i = i + size) {
+    for (let j = 0; j < size; j++) {
+      temp[j] = arr[j + i];
+      if (j + i === arr.length) {
+        temp.splice(j);
+        break;
+      }
+    }
+    chunkedArray = [...chunkedArray, [...temp]];
+  }
+  
+  return chunkedArray.slice(1);
+};
