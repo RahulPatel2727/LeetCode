7c82dd03523271bf1a3a6aedcef9f5a217f19a8d
Rahul Patel <rahul.patel19042000@gmail.com>
2023-06-25 01:41:30 +0530

    Added solution - LeetHub

---
+++ Unique rows in boolean matrix - GFG/unique-rows-in-boolean-matrix.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.lang.Math;
+import java.io.*;
+import java.util.*;
+class UniqueRows{
+public static void main(String[] args)throws IOException
+ {  
+     BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+     PrintWriter out=new PrintWriter(System.out);
+		int t=Integer.parseInt(in.readLine().trim());
+		while(t-->0){
+		    String s[]=in.readLine().trim().split(" ");
+			int r=Integer.parseInt(s[0]);
+			int c=Integer.parseInt(s[1]);
+			s=in.readLine().trim().split(" ");
+			int ind=0;
+			int[][] a=new int [r][c];
+			for(int i=0;i<r;i++)
+				for(int j=0;j<c;j++)
+					a[i][j]=Integer.parseInt(s[ind++]);
+			GfG g=new GfG();
+			ArrayList<ArrayList<Integer>> arr = g.uniqueRow(a,r,c);
+			for(int i = 0;i<arr.size();i++){
+			    ArrayList<Integer> demo = arr.get(i);
+			    for(int j = 0;j<demo.size();j++){
+			        out.print(demo.get(j)+" ");
+			    }
+			    out.print("$");
+			}
+			out.println();
+		}
+		out.close();
+ }
+}
+// } Driver Code Ends
+
+
+/*Complete the given function*/
+class GfG
+{
+    public static ArrayList<ArrayList<Integer>> uniqueRow(int arr[][],int r, int c)
+    {
+        //add code here.
+        HashSet<ArrayList<Integer>> set = new HashSet<>();
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int j=0;j<arr[0].length;j++){
+                temp.add(arr[i][j]);
+            }
+            if(!set.contains(temp)){
+                ans.add(temp);
+                set.add(temp);
+            }
+        }
+        return ans;
+    }
+}
