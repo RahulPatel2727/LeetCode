97786191f43b92d1049059f68d741c78c8597199
Rahul Patel <rahul.patel19042000@gmail.com>
2023-07-04 09:09:22 +0530

    Time: 2 ms (81.65%), Space: 44.4 MB (11.40%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -2,12 +2,12 @@ class Solution {
     public int singleNumber(int[] nums) {
         int ans = 0;
         for(int i=0;i<32;i++){
-            int ones = 0;
+            int o = 0;
             for(int n : nums){
-                ones += (n>>i)&1;
+                o += n>>i & 1;
             }
-            ones = ones % 3;
-            ans = ans | ones << i;
+            o = o % 3;//  0 or 1
+            ans = ans | o<<i;
         }
         return ans;
     }
