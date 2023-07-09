4d9a4f584f75464c3bd358d2d80c51e73d0a37eb
Rahul Patel <rahul.patel19042000@gmail.com>
2023-07-09 17:24:05 +0530

    Time: 1 ms (90.51%), Space: 41.4 MB (36.17%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,28 +1,29 @@
 class Solution {
-    public List<List<Integer>> generate(int n) {
+    public List<List<Integer>> generate(int numRows) {
+        return myFun(numRows);
+    }
+    public static List<List<Integer>> myFun(int n) {
         List<List<Integer>> ans = new ArrayList<>();
-        ans.add(new ArrayList<>(Arrays.asList(1)));
-        
-        int i = 2;
-        
-        while(i<=n){
+        int i = 1;
+        while (i <= n) {
             List<Integer> ds = new ArrayList<>();
-            List<Integer> pre = ans.get(i-2);
-            int size = pre.size();
-            int j = 0;
-            while(j<size){
-                if(j == 0){
-                    ds.add(1);
-                }
-                else{
-                    ds.add(pre.get(j)+pre.get(j-1));
-                }
+            int j = 1;
+            while (j <= i) {
+                ds.add(findValue(i - 1, j - 1));
                 j++;
             }
-            ds.add(1);
             ans.add(ds);
             i++;
         }
         return ans;
+    }
+
+    public static int findValue(int n, int r){
+        int val = 1;
+        int numerator = n, denominator = 1;
+        while(numerator > (n-r)){
+            val = (numerator-- * val / denominator++);
+        }
+        return val;
     }
 }
