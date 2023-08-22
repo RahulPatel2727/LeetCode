79f4d86f6a60c8ee9b6a1c4a5689da4cbd82cace
Rahul Patel <rahul.patel19042000@gmail.com>
2023-08-22 09:23:00 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (47.70%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder ans = new StringBuilder();
+        
+        while(columnNumber > 0) {
+            columnNumber--;
+            ans.append((char)((columnNumber) % 26 + 'A'));
+            columnNumber = (columnNumber) / 26;
+        }
+        return ans.reverse().toString();
+    }
+}
