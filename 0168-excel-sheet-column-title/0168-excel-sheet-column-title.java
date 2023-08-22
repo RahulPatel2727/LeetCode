5f032db9b6b5b0c3de9fa3147b5606401eac966c
Rahul Patel <rahul.patel19042000@gmail.com>
2023-08-22 17:59:24 +0530

    Time: 8 ms (15.49%), Space: 40.2 MB (29.14%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,12 +1,22 @@
 class Solution {
-    public String convertToTitle(int columnNumber) {
-        StringBuilder ans = new StringBuilder();
-        
-        while(columnNumber > 0) {
-            columnNumber--;
-            ans.append((char)((columnNumber) % 26 + 'A'));
-            columnNumber = (columnNumber) / 26;
+    public String convertToTitle(int n) {
+        String ans = "";
+        while(n > 0){
+            n--;
+            ans += (char)(n % 26 + 'A');
+            n = n / 26;
         }
-        return ans.reverse().toString();
+        ans = reverse(ans.toCharArray());
+        return ans;
+    }
+    public static String reverse(char [] arr){
+        int si = 0, ei = arr.length - 1;
+        while(si<ei){
+            char t = arr[si];
+            arr[si] = arr[ei];
+            arr[ei] = t;
+            si++; ei--;
+        }
+        return new String(arr);
     }
 }
