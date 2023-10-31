c99de525bb10329f461b976394197d9dc33b1dd7
Rahul Patel <rahul.patel19042000@gmail.com>
2023-10-31 16:56:11 +0530

    Time: 1 ms (100.00%), Space: 59.3 MB (39.89%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.java
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.java
@@ -4,8 +4,10 @@ class Solution {
         
         int[] arr = new int[n];
         arr[0] = pref[0];
+        int t = 0;
+        // let 8 ^ x = 9 ; then 8 ^ 9 = x 
         for (int i = 1; i < n; i++) {
-            arr[i] = pref[i] ^ pref[i - 1];
+            arr[i] = pref[i] ^ pref[i-1];
         }
         
         return arr;
