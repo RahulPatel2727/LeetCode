d87bff6dae016fea4047f504c3a8985aff4e55c0
Rahul Patel <rahul.patel19042000@gmail.com>
2023-11-04 14:43:48 +0530

    Time: 2 ms (20.88%), Space: 44.2 MB (9.89%) - LeetHub

---
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -0,0 +1,28 @@
+// class Solution {
+//     public int getLastMoment(int n, int[] left, int[] right) {
+//         int max = 0;
+//         for(int it : left){
+//             max = Math.max(it,max);
+//         }
+//         int min = Integer.MAX_VALUE;
+//         for(int it : right){
+//             min = Math.min(it, min);
+//         }
+        
+//     }
+// }
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int ans = 0;
+        for (int num : left) {
+            ans = Math.max(ans, num);
+        }
+        System.out.println(ans);
+        
+        for (int num : right) {
+            ans = Math.max(ans, n - num);
+        }
+        
+        return ans;
+    }
+}
