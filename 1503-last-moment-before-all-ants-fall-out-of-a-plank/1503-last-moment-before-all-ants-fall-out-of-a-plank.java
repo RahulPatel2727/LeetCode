f0dd6cd432e666ea37c5b178ef5b74965ffc27db
Rahul Patel <rahul.patel19042000@gmail.com>
2023-11-04 14:45:06 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (32.97%) - LeetHub

--- 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.java
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -1,28 +1,29 @@
-// class Solution {
-//     public int getLastMoment(int n, int[] left, int[] right) {
-//         int max = 0;
-//         for(int it : left){
-//             max = Math.max(it,max);
-//         }
-//         int min = Integer.MAX_VALUE;
-//         for(int it : right){
-//             min = Math.min(it, min);
-//         }
-        
-//     }
-// }
 class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
-        int ans = 0;
-        for (int num : left) {
-            ans = Math.max(ans, num);
+        int max = 0;
+        for(int it : left){
+            max = Math.max(it,max);
         }
-        System.out.println(ans);
-        
-        for (int num : right) {
-            ans = Math.max(ans, n - num);
+        int min = Integer.MAX_VALUE;
+        for(int it : right){
+            min = Math.min(it, min);
         }
+        return Math.max(max, n-min);
         
-        return ans;
     }
 }
+// class Solution {
+//     public int getLastMoment(int n, int[] left, int[] right) {
+//         int ans = 0;
+//         for (int num : left) {
+//             ans = Math.max(ans, num);
+//         }
+//         System.out.println(ans);
+        
+//         for (int num : right) {
+//             ans = Math.max(ans, n - num);
+//         }
+        
+//         return ans;
+//     }
+// }
