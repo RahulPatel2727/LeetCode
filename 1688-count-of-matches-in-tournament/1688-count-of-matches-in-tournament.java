33c2088feb0e8599b63549b9115f9e0f6e135877
Rahul Patel <rahul.patel19042000@gmail.com>
2023-12-05 10:33:11 +0530

    Time: 0 ms (100.00%), Space: 40 MB (5.39%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,10 +1,5 @@
 class Solution {
     public int numberOfMatches(int n) {
-        int ans = 0;
-        while(n>1){
-            ans += n/2;
-            n = (n+1)/2; 
-        }
-        return ans;
+        return n - 1;
     }
 }
