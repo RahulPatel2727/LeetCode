36d80b436a412c5994033c039855ecd0bd2037ba
Rahul Patel <rahul.patel19042000@gmail.com>
2023-12-07 23:12:10 +0530

    Time: 26 ms (41.31%), Space: 43.9 MB (79.30%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        return bs(piles, h);
+    }
+    static int bs(int []arr, int hrs){
+        int l = 1, h = max(arr);
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(possible(mid, arr, hrs)){
+                h = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+    static boolean possible(int mid, int []arr, int hrs){
+        int curHrs = 0;
+        for(int it : arr){
+            curHrs += Math.ceil(it/(mid*1.0));
+        }
+        return curHrs<=hrs;
+    }
+    static int max(int []arr){
+        int max = -1;
+        for(int it : arr){
+            max = max < it ? it : max;
+        }
+        return max;
+    }
+}
