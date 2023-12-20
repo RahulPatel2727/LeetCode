4b86bc23b5b8d1f73b5b31bfcc0c35f9f2faa34d
Rahul Patel <rahul.patel19042000@gmail.com>
2023-12-20 14:13:16 +0530

    Time: 2 ms (77.87%), Space: 44.8 MB (5.53%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int sum = prices[0]+prices[1];
+        if(sum>money)
+            return money;
+        return money - sum;
+    }
+}
