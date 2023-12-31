123cde939a620701488c133adba02adce5ef0271
Rahul Patel <rahul.patel19042000@gmail.com>
2023-12-31 12:36:13 +0530

    Time: 40 ms (24.86%), Space: 45.3 MB (6.35%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -2,47 +2,43 @@ class Solution {
     public int numberOfSubstrings(String s) {
         return total(s.length()) - count(s,2);
     }
-    static int count(String str, int k){
+    static int count(String str, int k) {
         int n = str.length();
         Set<Character> set = new HashSet<>();
-        int []f = new int[3];
-        int count = 0; int sum = 0;
+        int[] f = new int[3];
+        int count = 0;
         int si = 0;
-        for(int cur = 0; cur<n; cur++){
+        for (int cur = 0; cur < n; cur++) {
             char curCh = str.charAt(cur);
             f[curCh - 'a']++;
-            if(set.isEmpty()){
+            if (set.isEmpty()) {
                 set.add(curCh);
-                sum++;
-                count += 1;
-                continue;
             }
-            if(!set.contains(curCh)){
+            if (!set.contains(curCh)) {
                 set.add(curCh);
-                sum++;
             }
-            if(sum <= k){
-                count+=cur - si + 1;
+            if (set.size() <= k) {
+                count += cur - si + 1;
             }
             while (si <= cur && set.size() > k) {
                 char siCh = str.charAt(si);
-                f[siCh-'a']--;
-                if(f[siCh-'a']==0){
-                    sum--;
+                f[siCh - 'a']--;
+                if (f[siCh - 'a'] == 0) {
                     set.remove(siCh);
                 }
                 si++;
-                if(sum<=k){
-                    count+= cur - si + 1;
+                if (set.size() <= k) {
+                    count += cur - si + 1;
                 }
             }
         }
         return count;
     }
-    static int total(int len){
+
+    static int total(int len) {
         int sum = len;
         while(len-->0){
-            sum += len;
+            sum+=len;
         }
         return sum;
     }
