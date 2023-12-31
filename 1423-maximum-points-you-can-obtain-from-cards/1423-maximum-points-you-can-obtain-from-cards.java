994bfe20194e155ad813333392a7eba58f9d89ef
Rahul Patel <rahul.patel19042000@gmail.com>
2023-12-31 16:15:52 +0530

    Time: 3 ms (42.28%), Space: 55.6 MB (54.90%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,49 +1,24 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int []st = new int[k];
-        int []en = new int[k];
-        return preSum(cardPoints, k,st,en);
+        return preSum(cardPoints, k);
     }
-    static int preSum(int[] arr, int k, int[] st, int[] en) {
+    static int preSum(int[] arr, int k) {
         int n = arr.length;
         int maxSum = 0;
-        st[0] = arr[0];
-        for (int i = 1; i < k; i++) {
-            st[i] = st[i - 1] + arr[i];
+        int stSum = 0;
+        for (int i = 0; i < k; i++) {
+            stSum += arr[i];
         }
-        en[0] = arr[n - 1];
-        int it = 1;
-        for (int i = n - 2; i > n - k - 1; i--) {
-            en[it] = arr[i] + en[it - 1];
-            it++;
-        }
-        maxSum = Math.max(st[k - 1], en[k - 1]);
-        // maxSum = Math.max(maxSum, startEndMax(arr, k));
-
-        int itr = 0;
-        for (int i = k - 2; i >= 0; i--) {
-            maxSum = Math.max(maxSum, st[itr] + en[i]);
-            itr++;
+        maxSum = stSum;
+        int eiSum = 0;
+        int itr = k-1;
+        for (int i = n - 1; i > n - k - 1; i--) {
+            eiSum += arr[i];
+            stSum -= arr[itr];
+            maxSum = Math.max(maxSum, eiSum + stSum);
+            itr--;
         }
         return maxSum;
-    }
-    
-    // we don't need this i can optmise by removing it 
-    static int startEndMax(int[] arr, int k) {
-        int n = arr.length;
-        int si = 0, ei = n - 1;
-        int sum = 0;
-        while (si <= ei && k > 0) {
-            if (arr[si] >= arr[ei]) {
-                sum += arr[si];
-                si++;
-            } else {
-                sum += arr[ei];
-                ei--;
-            }
-            k--;
-        }
-        return sum;
     }
 }
 
