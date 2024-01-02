bf8dd4a22cddb557e639fc053db562cb4c80f559
Rahul Patel <rahul.patel19042000@gmail.com>
2024-01-02 18:14:22 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (11.09%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,29 +1,64 @@
-public class MyStack {
-    private Queue<Integer> q;
-
+class MyStack {
+    Queue2 q;
+    Queue2 t;
     public MyStack() {
-        q = new LinkedList<>();
+        q = new Queue2(100);
+        t = new Queue2(100);
     }
-
+    
     public void push(int x) {
-        q.add(x);
-        for (int i = 1; i < q.size(); i++) {
-            q.add(q.remove());
+        t.push(x);
+        for(int i=q.f; i<q.r;i++){
+            t.push(q.pop());
+        }
+        for(int i=t.f;i<t.r;i++){
+            q.push(t.pop());
         }
     }
-
+    
     public int pop() {
-        return q.remove();
+        return q.pop();
     }
-
+    
     public int top() {
-        return q.peek();
+        return q.top();
     }
-
+    
     public boolean empty() {
         return q.isEmpty();
     }
 }
+class Queue2{
+    int f, r, n, eCount=0;
+    int []arr;
+    Queue2(int size){
+        arr = new int[size];
+        f = 0; r = 0; n = size;
+    }
+    void push(int val){
+        if(eCount < n){
+            arr[r] = val;
+            r++;
+            eCount++;
+        }
+    }
+    int pop(){
+        if(eCount>0) {
+            int d = arr[f];
+            f++;
+            eCount--;
+            return d;
+        }
+        return 0;
+    }
+    int top(){
+        return arr[f];
+    }
+    boolean isEmpty(){
+        return eCount == 0;
+    }
+}
+
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack obj = new MyStack();
