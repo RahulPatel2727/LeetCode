ec3cce42352cb6273b10e4283d1614d5c7a8f82c
Rahul Patel <rahul.patel19042000@gmail.com>
2024-03-04 22:07:36 +0530

    Time: 2 ms (97.13%), Space: 42.1 MB (98.41%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int st = 0,en = tokens.length-1;
+        int score=0;
+        while(st<=en){
+            if(power>=tokens[st]){
+                power-=tokens[st];
+                ++score;
+                ++st;
+            }
+            else {
+           if(st!=en && score>0) {
+                    power+=tokens[en];
+                    // en--;
+                    score--;
+                }
+                en--;
+            }
+        }
+         return score>0?score:0;
+        
+    }
+
+}
