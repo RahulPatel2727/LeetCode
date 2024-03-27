040a3aba8e033a2036ccd17ae0c770215e32ded7
Rahul Patel <rahul.patel19042000@gmail.com>
2024-03-27 07:19:46 +0530

    Time: 1 ms (97.94%), Space: 54.4 MB (98.61%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,27 +1,25 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
-        int i = 0;
-        while (i < n) {
-            int correctIdx = nums[i] - 1;
-            if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[correctIdx]) {
-                swap(nums, i, correctIdx);
-            } else {
-                i++;
+        for(int i=0;i<n;){
+            int ind = nums[i]-1;
+            if(nums[i]>0 && nums[i]<=n && nums[ind] != nums[i]){
+                int t = nums[ind];
+                nums[ind] = nums[i];
+                nums[i] = t;
+            }else{
+                i++;   
             }
         }
-        System.out.println(Arrays.toString(nums));
-        
-        for (i = 0; i < n; i++) {
-            if (nums[i] != i + 1) {
-                return i + 1;
+        // System.out.println(Arrays.toString(nums));
+        int ans = 1;
+        for(int val : nums){
+            if(val != ans){
+                return ans;
+            }else{
+                ans++;
             }
         }
-        return n+1;
-    }
-    public void swap(int[] nums, int index1, int index2) {
-        int temp = nums[index1];
-        nums[index1] = nums[index2];
-        nums[index2] = temp;
+        return ans;
     }
 }
